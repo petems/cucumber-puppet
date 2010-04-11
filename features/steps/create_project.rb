e23496abe7acc7d9e9a2356d06b8f482c1f703d5
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 20:35:46 +0200

    finish creation tests

--- features/steps/create_project.rb
+++ features/steps/create_project.rb
@@ -7,12 +7,15 @@ Given /^cucumber\-puppet is installed$/ do
 end
 
 When /^I create a new project called "([^\"]*)"$/ do |project|
-  @project = project
-  fail if File.exists?("/tmp/#{@project}")
-  fail unless system("cd /tmp && cucumber-puppet-gen project #{@project}")
+  @project = "/tmp/#{project}"
+  fail if File.exists?(@project)
+  fail unless system("cd /tmp && cucumber-puppet-gen project #{project}")
 end
 
 When /^I freeze in dependencies$/ do
-  @project.should_not be_nil
+  fail unless system("cd #{@project} && gem bundle")
 end
 
+Then /^my gems directory should be populated$/ do
+  Dir.glob("#{@project}/vendor/gems/*").size.should > 0
+end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,6 @@
 After do
-  if File.exists?("/tmp/#{@project}")
-    FileUtils.rm_rf("/tmp/#{@project}")
+  if File.exists?(@project)
+    FileUtils.rm_rf(@project)
   end
   system("gem uninstall -x cucumber-puppet")
 end
