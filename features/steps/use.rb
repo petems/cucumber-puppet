934e9e97abadba6a8ae0029022d84b42ec358a75
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 21:15:37 +0200

    test for existence of generated feature file

--- features/steps/use.rb
+++ features/steps/use.rb
@@ -1,3 +1,7 @@
 When /^I generate a new feature called "([^\"]*)" for module "([^\"]*)"$/ do |feature, mod|
   fail unless system("cd #{@project} && ./bin/cucumber-puppet-gen feature #{mod} #{feature}")
 end
+
+Then /^a feature file for "([^\"]*)" in module "([^\"]*)" should exist$/ do |feature, mod|
+  fail unless File.exists?("#{@project}/features/modules/#{mod}/#{feature}.feature")
+end
