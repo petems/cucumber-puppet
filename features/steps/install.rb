27de60821cb16418f6b704a79c8d62a135cf67eb
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 15:06:05 +0200

    add steps to install and verify gem

--- features/steps/install.rb
+++ features/steps/install.rb
@@ -6,3 +6,15 @@ When /^I build the gem$/ do
   fail unless File.exist?(specfile)
   fail unless system("rake -f #{rakefile} build > /dev/null")
 end
+
+When /^I install the latest gem$/ do
+  project_root = File.join(File.dirname(__FILE__), '..', '..')
+  pkg_dir = File.join(project_root, 'pkg')
+  pkg = File.expand_path(Dir.glob(File.join(pkg_dir, '*.gem')).last)
+
+  fail unless system("gem install #{pkg} > /dev/null 2>&1")
+end
+
+Then /^I should have cucumber\-puppet\-gen in my PATH$/ do
+  fail unless system("which cucumber-puppet-gen")
+end
