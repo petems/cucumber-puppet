7936e29d3e71e5703c39d55070536b16fd74d8a9
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 14:59:29 +0200

    fail unless gemspec

--- features/steps/install.rb
+++ features/steps/install.rb
@@ -2,5 +2,7 @@ When /^I build the gem$/ do
   project_root = File.join(File.dirname(__FILE__), '..', '..')
   rakefile = File.join(project_root, 'Rakefile')
   fail unless File.exist?(rakefile)
-  fail unless system("rake -f #{rakefile} build")
+  specfile = File.join(project_root, 'cucumber-puppet.gemspec')
+  fail unless File.exist?(specfile)
+  fail unless system("rake -f #{rakefile} build > /dev/null")
 end
