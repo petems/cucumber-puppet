1b795ec13292729c5de966db90f2a04dc72125af
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 20:11:22 +0200

    test project creation

--- features/steps/create_project.rb
+++ features/steps/create_project.rb
@@ -5,3 +5,14 @@ Given /^cucumber\-puppet is installed$/ do
     Then I should have cucumber-puppet-gen in my PATH
   }
 end
+
+When /^I create a new project called "([^\"]*)"$/ do |project|
+  @project = project
+  fail if File.exists?("/tmp/#{@project}")
+  fail unless system("cd /tmp && cucumber-puppet-gen project #{@project}")
+end
+
+When /^I freeze in dependencies$/ do
+  @project.should_not be_nil
+end
+
