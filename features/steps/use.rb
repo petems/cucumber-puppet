5f459035b67bb649703b0de9442b7cd00c9296d0
Nikolay Sturm <github@erisiandiscord.de>
2010-04-11 21:08:00 +0200

    typos

--- features/steps/use.rb
+++ features/steps/use.rb
@@ -1,3 +1,3 @@
 When /^I generate a new feature called "([^\"]*)" for module "([^\"]*)"$/ do |feature, mod|
-  fail unless system("cd #{@project} && ./bin/cucumber-nagios-gen feature #{mod} #{feature}")
+  fail unless system("cd #{@project} && ./bin/cucumber-puppet-gen feature #{mod} #{feature}")
 end
