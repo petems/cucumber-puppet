38357589ea38709d208c91f216b791ca5aaf8fba
Nikolay Sturm <github@erisiandiscord.de>
2010-04-16 07:20:47 +0200

    dont interfere with the Cucumber module

--- lib/cucumber-puppet.rb
+++ lib/cucumber-puppet.rb
@@ -1,8 +1,6 @@
 require 'yaml'
 
-module Cucumber
-  class Puppet
-    version = YAML.load_file(File.dirname(__FILE__) + '/../VERSION.yml')
-    VERSION = [version[:major], version[:minor], version[:patch], version[:build]].compact.join('.')
-  end
+class CucumberPuppet
+  version = YAML.load_file(File.dirname(__FILE__) + '/../VERSION.yml')
+  VERSION = [version[:major], version[:minor], version[:patch], version[:build]].compact.join('.')
 end
