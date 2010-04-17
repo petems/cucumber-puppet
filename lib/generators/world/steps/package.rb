2c7b84e5b5c3f4bcdd02fdf28f8f432957a5174b
Nikolay Sturm <github@erisiandiscord.de>
2010-04-17 22:51:52 +0200

    remove useless step

--- lib/generators/world/steps/package.rb
+++ lib/generators/world/steps/package.rb
@@ -2,7 +2,6 @@ Then /^package "([^\"]*)" should be "([^\"]*)"$/ do  |package, state|
   steps %Q{
     Then there should be a resource "Package[#{package}]"
     And the state should be "#{state}"
-    And the package should require "Exec[update_apt]"
   }
 end
 
