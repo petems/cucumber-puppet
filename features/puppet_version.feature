89af1f34dea1f8782dfd771273e9bc9db7a3d6f2
Nikolay Sturm <github@erisiandiscord.de>
2010-04-21 20:10:52 +0200

    run testcase against different puppet versions

--- features/puppet_version.feature
+++ features/puppet_version.feature
@@ -1,10 +1,14 @@
-Feature: Test cucumber-puppet against current Puppet releases
+Feature: Test cucumber-puppet against various Puppet releases
   In order to support a broad user base
   As a developer of cucumber-puppet
-  I want cucumber-puppet to properly work with current Puppet releases
+  I want cucumber-puppet to properly work with various Puppet releases
 
-  Scenario: Test Puppet 0.24 branch
-    Given an initialized directory tree
-    When puppet version "0.24.8" is installed
-    And I generate "testcase"
+  Scenario Outline: Test specific Puppet version
+    Given an initialized directory tree for puppet version "<version>"
+    When I generate "testcase"
     Then cucumber-puppet should successfully run "testcase"
+
+    Examples:
+      | version |
+      | 0.24.8 |
+      | 0.24.9 |
--- features/steps/initialization.rb
+++ features/steps/initialization.rb
@@ -11,9 +11,30 @@ Given /^an initialized directory tree$/ do
   }
 end
 
+Given /^an initialized directory tree for puppet version "([^\"]*)"$/ do |version|
+  steps %Q{
+    Given an uninitialized directory tree
+    When I generate "world" for puppet version "#{version}"
+  }
+end
+
 When /^I generate "([^\"]*)"$/ do |generator|
   basedir = File.dirname(__FILE__) + "/../.."
   fail unless system("ruby -I#{basedir}/lib #{basedir}/bin/cucumber-puppet-gen #{generator} > /dev/null")
+  if generator == "world"
+    oldworld = IO.read("features/support/world.rb")
+    newworld = []
+    newworld << "$LOAD_PATH.unshift '#{basedir}/lib'\n"
+    newworld << oldworld
+    world = File.open("features/support/world.rb", "w")
+    world << newworld
+    world.close
+  end
+end
+
+When /^I generate "([^\"]*)" for puppet version "([^\"]*)"$/ do |generator, version|
+  basedir = File.dirname(__FILE__) + "/../.."
+  fail unless system("ruby -I#{basedir}/lib #{basedir}/bin/cucumber-puppet-gen #{generator} #{version}")
   if generator == "world"
     oldworld = IO.read("features/support/world.rb")
     newworld = []
