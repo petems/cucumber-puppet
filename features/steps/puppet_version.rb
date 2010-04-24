d8b4b5ca60a6a52ef8eb231ecd53e72d02654ccf
Nikolay Sturm <github@erisiandiscord.de>
2010-04-24 17:35:36 +0200

    include lib dir for cucumber-puppet

--- features/steps/puppet_version.rb
+++ features/steps/puppet_version.rb
@@ -1,6 +1,6 @@
 Then /^cucumber\-puppet should successfully run "([^\"]*)"$/ do |feature|
   basedir = File.dirname(__FILE__) + '/../..'
-  output = `ruby #{basedir}/bin/cucumber-puppet #{feature}`
+  output = `ruby -I#{basedir}/lib  #{basedir}/bin/cucumber-puppet #{feature}`
   if $?.to_i > 0
     puts output
     fail
