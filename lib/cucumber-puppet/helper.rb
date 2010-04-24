42eafcf3288030b14e1466a46de649df52f418e6
Nikolay Sturm <github@erisiandiscord.de>
2010-04-24 20:40:03 +0200

    reorder and comment for clarity

--- lib/cucumber-puppet/helper.rb
+++ lib/cucumber-puppet/helper.rb
@@ -1,19 +1,30 @@
 require 'cucumber-puppet'
 
 module CucumberPuppet::Helper
+  # Finds the root of the puppet directory tree according to puppet's
+  # directory naming conventions. Relevant sub-trees are:
+  # - <puppetdir>/features/modules/$module
+  # - <puppetdir>/modules/$module/features
+  # In case no tree is identified, assumes cwd is <puppetdir>.
   def find_root(cwd)
     path = cwd.split('/')
-    if cwd.match('modules$') and not cwd.match('/features/')
-      path.pop
-    elsif cwd.match('/modules/[^/]*$') and not cwd.match('/features/')
-      path.pop(2)
-    elsif cwd.match('/modules/[^/]*/features$')
-      path.pop(3)
-    elsif cwd.match('features$')
+    if cwd.match('features$') and not cwd.match("modules/")
+      # <puppetdir>/features
       path.pop
     elsif cwd.match('features/modules$')
+      # <puppetdir>/features/modules
       path.pop(2)
     elsif cwd.match('features/modules/[^/]*$')
+      # <puppetdir>/features/modules/$module
+      path.pop(3)
+    elsif cwd.match('modules$')
+      # <puppetdir>/modules
+      path.pop
+    elsif cwd.match('/modules/[^/]*$')
+      # <puppetdir>/modules/$module
+      path.pop(2)
+    elsif cwd.match('/modules/[^/]*/features$')
+      # <puppetdir>/modules/$module/features
       path.pop(3)
     end
     path.join('/')
