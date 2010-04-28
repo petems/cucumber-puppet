c4a2b10df97182732423a3b509436606bc374023
Nikolay Sturm <github@erisiandiscord.de>
2010-04-28 12:08:48 +0200

    specs for find_destdir

--- spec/cucumber-puppet/helper_spec.rb
+++ spec/cucumber-puppet/helper_spec.rb
@@ -2,6 +2,24 @@ require 'cucumber-puppet/helper'
 include CucumberPuppet::Helper
 
 describe 'CucumberPuppet::Helper' do
+  describe '.find_destdir' do
+    paths = {
+      'puppet' => 'features/modules/foo',
+      'puppet/features' => 'modules/foo',
+      'puppet/features/modules' => 'foo',
+      'puppet/features/modules/foo' => '.',
+      'puppet/modules' => 'foo/features',
+      'puppet/modules/foo' => 'features',
+      'puppet/modules/foo/features' => '.',
+    }
+    paths.each do |cwd, dest|
+      it "should return #{dest} for #{cwd}" do
+        Dir.stub(:getwd).and_return(cwd)
+        destdir = find_destdir("foo")
+        destdir.should == dest
+      end
+    end
+  end
   describe '.find_root' do
     paths = [
       'puppet', 'puppet/modules', 'puppet/modules/foo',
