657d320a0e77c42dddbffe02786dfa36ec562e1b
Nikolay Sturm <github@erisiandiscord.de>
2010-05-18 10:06:26 +0200

    deal with tables of packages and remove useless step definition

--- lib/generators/world/steps/package.rb
+++ lib/generators/world/steps/package.rb
@@ -1,13 +1,14 @@
-Then /^package "([^\"]*)" should be "([^\"]*)"$/ do  |package, state|
+Then /^following packages should be dealt with:$/ do |packages|
+  packages.hashes.each do |package|
+    steps %Q{
+      Then package "#{package['name']}" should be "#{package['state']}"
+    }
+  end
+end
+
+Then /^package "([^\"]*)" should be "([^\"]*)"$/ do |package, state|
   steps %Q{
     Then there should be a resource "Package[#{package}]"
     And the state should be "#{state}"
-  }
-end
-
-
-Then /^package "([^\"]*)" should not be installed$/ do  |package|
-  steps %Q{
-    Then there should be no resource "Package[#{package}]"
   }
 end
