d1e5a01573d8a1e2edb2d2b23ef8923a5eedad05
Nikolay Sturm <github@erisiandiscord.de>
2010-05-18 20:58:39 +0200

    better user in groups check

--- lib/generators/world/steps/user.rb
+++ lib/generators/world/steps/user.rb
@@ -1,3 +1,15 @@
+Then /^"([^\"]*)" should be in groups? "([^\"]*)"$/ do |user, groups|
+  steps %Q{
+    Then there should be a resource "User[#{user}]"
+    And the user should be in groups "#{groups}"
+  }
+end
+
 Then /^the user should be in groups "([^\"]*)"$/ do |groups|
-  fail unless @resource["groups"] == groups
+  g = @resource["groups"]
+  g_s = g
+  if g.is_a?(Array)
+    g_s = g.join(' ')
+  end
+  fail unless g_s == groups
 end
