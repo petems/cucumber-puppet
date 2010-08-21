3345ec313eb8357da919da19778bc09a58139ef0
Nikolay Sturm <github@erisiandiscord.de>
2010-08-21 18:11:29 +0200

    use N.times { pop } instead of popping manually

--- lib/cucumber-puppet/helper.rb
+++ lib/cucumber-puppet/helper.rb
@@ -20,25 +20,19 @@ module CucumberPuppet::Helper
       path.pop
     elsif cwd.match('features/modules$')
       # <puppetdir>/features/modules
-      path.pop
-      path.pop
+      2.times { path.pop }
     elsif cwd.match('features/modules/[^/]*$')
       # <puppetdir>/features/modules/$module
-      path.pop
-      path.pop
-      path.pop
+      3.times { path.pop }
     elsif cwd.match('modules$')
       # <puppetdir>/modules
       path.pop
     elsif cwd.match('/modules/[^/]*$')
       # <puppetdir>/modules/$module
-      path.pop
-      path.pop
+      2.times { path.pop }
     elsif cwd.match('/modules/[^/]*/features$')
       # <puppetdir>/modules/$module/features
-      path.pop
-      path.pop
-      path.pop
+      3.times { path.pop }
     end
 
     path.join('/')
