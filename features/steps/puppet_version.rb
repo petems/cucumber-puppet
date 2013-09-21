18eb0d0c5b98367baf667cd5c0967ed90dcf7d89
petems <p.morsou@gmail.com>
2013-09-21 17:31:15 +0100

    Removing puppet version tests (done on Travis now)

--- features/steps/puppet_version.rb
+++
@@ -1,8 +0,0 @@
-Then /^cucumber\-puppet should successfully run "([^\"]*)"$/ do |feature|
-  basedir = File.dirname(__FILE__) + '/../..'
-  output = `ruby -I#{basedir}/lib  #{basedir}/bin/cucumber-puppet #{feature}`
-  if $?.to_i > 0
-    puts output
-    fail
-  end
-end
